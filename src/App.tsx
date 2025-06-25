a8a082204ec973d8dbc2c5d479a02fd0fcdaefd3
Andrea Morelli <andrea.morelli.reply@skytv.it>
2025-06-25 15:57:19 +0200

    first version of the portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import { useScrollAnimation, ScrollToTop } from './hooks';
 import { TypingAnimation, MagneticButton, useScrollReveal } from './components/AdvancedEffects';
